9a14b073a9de63a03c643ee2d2333f561a43b1a2
ph <pcholt@gmail.com>
2018-01-07 21:57:22 +1100

    Add a map activity

--- app/src/main/java/com/overswell/demoapplication/MainActivityFragment.kt
+++ app/src/main/java/com/overswell/demoapplication/MainActivityFragment.kt
@@ -1,10 +1,12 @@
 package com.overswell. demoapplication
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import kotlinx.android.synthetic.main.fragment_main.view.*
 
 /**
  * A placeholder fragment containing a simple view.
@@ -19,6 +21,10 @@ class MainActivityFragment : Fragment() {
         val view = inflater.inflate(R.layout.fragment_main, container, false)
 
         // Add data observations here
+
+        view.button_map.setOnClickListener {
+            startActivity(Intent(activity, MapsActivity::class.java))
+        }
 
         return view
     }
---
+++ app/src/main/java/com/overswell/demoapplication/MapsActivity.kt
@@ -0,0 +1,26 @@
+package com.overswell.demoapplication
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+
+class MapsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_maps)
+
+        (supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment).getMapAsync {
+            mMap ->
+
+            // Add a marker in Sydney and move the camera
+            val sydney = LatLng(-34.0, 151.0)
+            mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
+            mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+        }
+    }
+
+}
