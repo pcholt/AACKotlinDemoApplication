7c9ccb362eaf206a5bd464e50f32572e96cd6642
pcholt <pcholt@gmail.com>
2018-01-26 12:36:15 +1100

    blank branch created - add new projects from here

--- app/src/main/java/com/overswell/demoapplication/MainActivity.kt
+++ app/src/main/java/com/overswell/demoapplication/MainActivity.kt
@@ -1,12 +1,8 @@
 package com.overswell.demoapplication
 
-import android.arch.lifecycle.MutableLiveData
 import android.arch.lifecycle.ViewModel
 import android.arch.lifecycle.ViewModelProviders
 import android.content.Context
-import android.hardware.Sensor
-import android.hardware.SensorEvent
-import android.hardware.SensorEventListener
 import android.hardware.SensorManager
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
@@ -15,70 +11,19 @@ import kotlinx.android.synthetic.main.activity_main.*
 class MainActivity : AppCompatActivity() {
 
     private val viewModel by lazy {
-        ViewModelProviders.of(this).get(SensorViewModel::class.java)
-    }
-
-    private val sensorManager by lazy {
-        application.getSystemService(Context.SENSOR_SERVICE) as SensorManager
-    }
-
-    private val sensor: Sensor? by lazy {
-        sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)
-    }
-
-    private val sensorEventListener by lazy {
-        object : SensorEventListener {
-
-            override fun onAccuracyChanged(sensor1: Sensor?, newAccuracy: Int) {
-                viewModel.accuracyChange(newAccuracy)
-            }
-
-            override fun onSensorChanged(sensorEvent: SensorEvent?) {
-                viewModel.sensorChange(sensorEvent?.values)
-            }
-        }
+        ViewModelProviders.of(this).get(MainViewModel::class.java)
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
-    }
 
-    override fun onResume() {
-        super.onResume()
-        sensorManager.registerListener(sensorEventListener,
-                sensor, SensorManager.SENSOR_DELAY_GAME)
-    }
-
-    override fun onPause() {
-        sensorManager.unregisterListener(sensorEventListener)
-        super.onPause()
     }
 
 }
 
-class SensorViewModel : ViewModel() {
-
-    val data: MutableLiveData<SensorData> = MutableLiveData()
-    val accuracy: MutableLiveData<Int> = MutableLiveData()
-    val sensorCache = SensorData()
-
-    fun sensorChange(values: FloatArray?) {
-        sensorCache.fromSensor(values)
-        data.value = sensorCache
-    }
-
-    fun accuracyChange(accuracy: Int) {
-        this.accuracy.value = accuracy
-    }
+class MainViewModel : ViewModel() {
 
 }
 
-data class SensorData(var x:Float=0f, var y:Float=0f, var z:Float=0f) {
-    fun fromSensor(data:FloatArray?) {
-        if (data?.size?:0 > 0) x = data?.get(0) ?: 0f
-        if (data?.size?:0 > 1) y = data?.get(1) ?: 0f
-        if (data?.size?:0 > 2) z = data?.get(2) ?: 0f
-    }
-}
--- app/src/main/java/com/overswell/demoapplication/MainActivityFragment.kt
+++ app/src/main/java/com/overswell/demoapplication/MainActivityFragment.kt
@@ -4,6 +4,7 @@ import android.arch.lifecycle.Observer
 import android.arch.lifecycle.ViewModelProviders
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.support.v4.app.FragmentActivity
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -14,10 +15,10 @@ import kotlinx.android.synthetic.main.fragment_main.view.*
  */
 class MainActivityFragment : Fragment() {
 
-    private val viewModel: SensorViewModel by lazy {
-        ViewModelProviders
-                .of(activity)
-                .get(SensorViewModel::class.java)
+    private val viewModel: MainViewModel by lazy {
+            ViewModelProviders
+                    .of(activity!!)
+                    .get(MainViewModel::class.java)
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
@@ -26,18 +27,6 @@ class MainActivityFragment : Fragment() {
         val view = inflater.inflate(R.layout.fragment_main, container, false)
 
         // Add viewmodel data observations here
-
-        viewModel.data.observe(activity, Observer {
-
-            view.textView_x.text = "${it?.x}"
-            view.textView_y.text = "${it?.y}"
-            view.textView_z.text = "${it?.z}"
-
-            view.imageView.translationX = 100f * (it?.y ?: 0f)
-            view.imageView.translationY = 100f * (it?.x ?: 0f)
-            view.imageView.rotation = 10f* (it?.z ?: 0f)
-
-        })
 
         return view
     }
